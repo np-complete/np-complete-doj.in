8f30f071ee3653fb1ce63595497255df02c7ef7e
masarakki <masaki182@gmail.com>
2020-01-13 07:22:28 +0900

    use-webfont

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -5,8 +5,8 @@ import Books from './books';
 const Main = () => (
   <section className="section">
     <div className="container">
-      <h1 className="title">NP-complete</h1>
-      <p className="subtitle">プログラミング・技術書系同人サークル</p>
+      <h1 id="title" className="title">NP-complete</h1>
+      <p id="subtitle" className="subtitle">プログラミング・技術書系同人サークル</p>
       <Books />
     </div>
   </section>
