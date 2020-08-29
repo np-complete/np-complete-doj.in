8c9ef43d72d0c8c01dc3d999fd05a3113a03b154
masarakki <masaki182@gmail.com>
2020-08-29 21:41:04 +0900

    add-stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: [
+    'stylelint-config-prettier',
+    'stylelint-config-recommended-scss',
+    'stylelint-config-sass-guidelines',
+  ],
+  plugins: ['stylelint-order', 'stylelint-prettier'],
+  rules: {
+    'order/properties-alphabetical-order': true,
+    'prettier/prettier': true,
+    'selector-max-id': 1,
+  },
+};
