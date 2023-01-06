799f7172ef494b8a9dbd3acad78b350d32c9942a
masarakki <masaki182@gmail.com>
2023-01-07 01:35:43 +0900

    add-cloudfront

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -4,3 +4,30 @@ resource "aws_s3_bucket" "np-complete-website" {
     Project = "np-complete"
   }
 }
+
+resource "aws_s3_bucket_policy" "np-complete-website" {
+  bucket = aws_s3_bucket.np-complete-website.id
+  policy = data.aws_iam_policy_document.np-complete-website.json
+
+}
+
+data "aws_iam_policy_document" "np-complete-website" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+    actions = [
+      "s3:GetObject"
+    ]
+    resources = [
+      "${aws_s3_bucket.np-complete-website.arn}/*"
+    ]
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.np-complete.arn]
+    }
+  }
+}
